0414f5afe4d11c7473927bbc382bba50c6ff73e9
Nihaal Sangha <nihaal.git@gmail.com>
2021-03-04 10:57:38 +0000

    Add metadata

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,20 @@
 import { Box, Button, Heading, Text, useColorMode } from "@chakra-ui/react";
+import Head from "next/head";
 
 export default function Home(): JSX.Element {
   const { colorMode, toggleColorMode } = useColorMode();
   return (
-    <Box p={2}>
-      <Heading as="h1">Hello, World!</Heading>
-      <Text>Currently in {colorMode} mode</Text>
-      <Button onClick={() => toggleColorMode()}>Switch to {colorMode === "light" ? "dark" : "light"} mode</Button>
-    </Box>
+    <>
+      <Head>
+        <title>Hypothesis testing solver</title>
+        <meta property="og:title" content="Hypothesis testing solver" key="title" />
+        <meta name="description" content="A hypothesis testing solver which includes working out" key="description" />
+      </Head>
+      <Box p={2}>
+        <Heading as="h1">Hello, World!</Heading>
+        <Text>Currently in {colorMode} mode</Text>
+        <Button onClick={() => toggleColorMode()}>Switch to {colorMode === "light" ? "dark" : "light"} mode</Button>
+      </Box>
+    </>
   );
 }
