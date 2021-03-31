4364f9964ca7504cd7e83949f95baf6a17846bc8
Nihaal Sangha <nihaal.git@gmail.com>
2021-04-01 00:19:22 +0100

    Fix web worker not loading in production

--- next.config.js
+++ next.config.js
@@ -2,4 +2,11 @@ module.exports = {
   future: {
     webpack5: true,
   },
+  // See https://github.com/vercel/next.js/issues/22813
+  webpack: (config, { isServer, dev }) => {
+    config.output.chunkFilename = isServer
+      ? `${dev ? "[name]" : "[name].[fullhash]"}.js`
+      : `static/chunks/${dev ? "[name]" : "[name].[fullhash]"}.js`;
+    return config;
+  },
 };
